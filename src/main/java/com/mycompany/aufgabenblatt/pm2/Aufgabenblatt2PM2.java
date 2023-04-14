7990ab90d5b0fff20419e8de2cebd0a94c14493f
Maurice Haghighi Pajouh <94959873+MauriceHaghighiPajouh@users.noreply.github.com>
2023-04-14 18:32:26 +0200

    Blatt 2 empty

---
+++ src/main/java/com/mycompany/aufgabenblatt/pm2/Aufgabenblatt2PM2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aufgabenblatt.pm2;
+
+/**
+ *
+ * @author mauricehaghighi
+ */
+public class Aufgabenblatt2PM2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
